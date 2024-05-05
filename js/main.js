b7b22bd024b1664c59e989ede9d5dda10cd93d11
Joshua Donahue <joshua.a.donahue@gmail.com>
2024-05-05 15:46:30 -0400

    Build footer component.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// Collapsibles
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+// Copyright Year
+document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
